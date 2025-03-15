64c33e9dc4252ca56d88dd85786e32a0939c181a
Ganesh Bhagat <gbhagat1512@gmail.com>
2025-03-15 17:50:55 +0530

    Updated README

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+
+   '''
+   this function will return a list of requirements
+   '''
+
+   hypen_e_dot='-e .'
+   requirements=[]
+   with open (file_path) as file_obj:
+     requirements=file_obj.readlines()
+     requirements=[req.replace("\n","")for req in requirements]
+
+     if hypen_e_dot in requirements:
+        requirements.remove(hypen_e_dot)
+
+   return requirements
+
+
+
+setup(
+name='mlproject',
+vesrsion='0.0.1',
+author_email='gbhagat1512@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
